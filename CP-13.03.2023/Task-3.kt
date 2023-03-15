9835a78e5e494c6e339dfea56534a42da9645686
BetlBit <inthebetmen@mail.ru>
2023-03-15 04:04:45 +0300

    Task3
    [I@3d4eac69

---
+++ CP-13.03.2023/Task-3.kt
@@ -0,0 +1,13 @@
+fun main() {
+	var customerArray = CustomerArray()
+	customerArray.createArr(5, 1) // Выводит [I@3d4eac69
+}
+
+class CustomerArray {
+	fun createArr(length: Int, untilNumber: Int) : IntArray {
+		// Примитивный тип массива
+		var arr = IntArray(length) { it * untilNumber } // Должен вывести 0, 1, 2, 3, 4
+		println(arr)
+		return arr;
+	}
+}
