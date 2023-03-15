7c780e9dc867dfab591cb7c77ccb6d05340f1a80
BetlBit <inthebetmen@mail.ru>
2023-03-15 03:00:43 +0300

    Task2
    Final

---
+++ CP-13.03.2023/Task-2.kt
@@ -0,0 +1,16 @@
+fun main() {
+	var array: Array<Int> = arrayOf(1, 2, 9) // Впишите нужный массив
+	var goal: Int = 10 // Впишите заданное число
+
+	println(isSumNumber(array, goal))
+}
+
+fun isSumNumber(arr: Array<Int>, goal: Int, i: Int = 0) : Boolean {
+	var n: Int = arr.size
+		
+	if (goal <= 0 || i >= n) {
+		return goal == 0;
+	} else {
+		return isSumNumber(arr, goal - arr[i], i + 1) || isSumNumber(arr, goal, i + 1);
+	}
+}
